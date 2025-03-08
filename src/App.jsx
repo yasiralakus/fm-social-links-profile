33d2a8d56091d961d61d3476ccfc9076aac23b7c
yasiralakus <yasiralakus11@gmail.com>
2025-03-09 00:46:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+export default function App() {
+
+
+  return (
+    <div className="full-page">
+
+        <div className="main-box">
+
+            <img src="./images/yasiralakus.jpg" alt="" />
+
+            <div className="about">
+                <h3>Yasir Alakuş</h3>
+                <h5>Istanbul, Turkiye</h5>
+            </div>
+
+            <p>"Frontend and Mobile App Developer"</p>
+
+            <div className="social-links">
+                <a href="https://github.com/yasiralakus">GitHub</a>
+                <a href="https://www.frontendmentor.io/profile/yasiralakus">Frontend Mentor</a>
+                <a href="https://www.linkedin.com/in/yasiralakus/">Linkedin</a>
+                <a href="https://www.x.com/ysroloji">Twitter</a>
+                <a href="https://www.instagram.com/yasiralakus">Instagram</a>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
